5dd39ac5411b0ed103d8603b38768230c1372b9d
Doğukan Kökce <88978496+dgknkokce@users.noreply.github.com>
2023-03-20 13:15:43 +0300

    header file created and updated for lang specification

---
+++ mylangcommon.h
@@ -0,0 +1,26 @@
+#define IDENTIFIER 1
+#define INTEGER 2
+#define DOUBLE 3
+#define EQUALS 4
+#define WRITE 5
+#define PAROPEN 6
+#define PARCLOSE 7
+#define EXIT 8
+#define ADD 9
+#define MULTIPLY 10
+#define SUBTRACT 11
+#define DIVIDE 12
+#define SEMICOLON 13
+#define BOOLEAN 14
+#define FUNCTION 15
+#define CBOPEN 16
+#define CBCLOSE 17
+#define RETURN 18
+#define IF 19
+#define ELSEIF 20
+#define WHILE 21
+#define FOR 22
+#define ELSE 23
+#define LOWER 24
+#define GREATER  25
+#define DEFINETOKENS char *text[] = {NULL,"IDENTIFIER", "INTEGER", "DOUBLE", "EQUALS" ,"WRITE" ,"PAROPEN", "PARCLOSE" ,"EXIT", "ADD", "MULTIPLY", "SUBTRACT", "DIVIDE","SEMICOLON" ,"BOOLEAN", "FUNCTION", "CBOPEN", "CBCLOSE", "RETURN","IF", "ELSEIF", "WHILE", "FOR", "ELSE","LOWER","GREATER"};
